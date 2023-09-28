440bfa07cdcf5426b363ad73e0b8fbae6eca6df0
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-09-28 16:37:42 +0000

    decorated endpoint responses with flask smorest

--- resources/store.py
+++ resources/store.py
@@ -3,13 +3,14 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
-from schemas import StoreSchema
+from schemas import StoreSchema, StoreUpdateSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -23,13 +24,9 @@ class Store(MethodView):
         except:
             abort(404, message="Store not found.")
 
-    def put(self, store_id):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price'is included in the json payload.",
-            )
+    @blp.arguments(StoreUpdateSchema)
+    @blp.response(200, StoreSchema)
+    def put(self, store_data, store_id):
         try:
             store = stores[store_id]
             store |= store_data
@@ -41,10 +38,12 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
 
     @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
     def post(self, store_data):
         for store in stores:
             if store_data["name"] == store["name"]:
--- schemas.py
+++ schemas.py
@@ -16,3 +16,7 @@ class ItemUpdateSchema(Schema):
 class StoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
+
+
+class StoreUpdateSchema(Schema):
+    name = fields.Str()
