497a14b3347934708e6ac99549d457cf71eac27b
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-09-28 12:59:30 +0000

    added delete and update endpoints for items and stores

--- app.py
+++ app.py
@@ -64,9 +64,61 @@ def get_store(store_id):
         abort(404, message="Store not found.")
 
 
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "name" not in item_data or "price" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price' and 'name' are included in the json payload.",
+        )
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except:
+        abort(404, message="Item not found")
+
+
 @app.get("/item/<string:item_id>")
-def get_items(item_id):
+def get_item(item_id):
     try:
         return items[item_id]
     except:
         abort(404, message="Item not found.")
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "name" not in item_data or "price" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price' and 'name' are included in the json payload.",
+        )
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except:
+        abort(404, message="Item not found")
