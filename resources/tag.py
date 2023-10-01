1de6888dc9620ee0116de96be216487994b3b917
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-10-01 21:52:08 +0000

    added and updated route to link and unlink tags to items

--- resources/tag.py
+++ resources/tag.py
@@ -2,9 +2,9 @@ from flask.views import MethodView
 from sqlalchemy.exc import SQLAlchemyError
 from flask_smorest import abort, Blueprint
 
-from models import TagModel, StoreModel
+from models import TagModel, StoreModel, ItemModel
 from db import db
-from schemas import TagSchema
+from schemas import TagSchema, TagAndItemSchema
 
 
 blp = Blueprint("Tags", "tags", description="Operations on tags")
@@ -36,3 +36,58 @@ class Tag(MethodView):
     def get(self, tag_id):
         tag = TagModel.query.get_or_404(tag_id)
         return tag
+
+    @blp.response(
+        202,
+        description="Deletes a tag if no item is tagged with it.",
+        example={"message": "Tag Deleted"},
+    )
+    @blp.alt_response(404, description="Tag not found")
+    @blp.alt_response(
+        400,
+        description="Returns if the tag is assigned to one or more items. In this case the tah=g is nit==ot deleted",
+    )
+    def delete(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return {"message": "Tag deleted"}
+        abort(
+            400,
+            message="Could not delete tag. Make sure tag is not associated with any items, then try again.",
+        )
+
+
+@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+class LinkTagsToItem(MethodView):
+    @blp.response(201, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.append(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+
+        return tag
+
+    @blp.response(200, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.remove(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+
+        return {"message": "Item removed from tag", "item": item, "tag": tag}
