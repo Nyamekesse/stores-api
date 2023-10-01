5b4553d863c0b0062be0993d72d4f1ca8b04ced7
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-10-01 21:08:03 +0000

    refactored code to include model relationships to items_tags model

--- models/tag.py
+++ models/tag.py
@@ -8,3 +8,4 @@ class TagModel(db.Model):
     name = db.Column(db.String(80), unique=False, nullable=False)
     stored_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
     store = db.relationship("StoreModel", back_populates="tags")
+    items = db.relationship("ItemModel", back_populates="tags", secondary="items_tags")
