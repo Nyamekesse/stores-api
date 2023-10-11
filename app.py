d65449ac4116f3fcfe9523139a7bcf25ccca6ba6
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-10-11 08:56:07 +0000

    defined user schema

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 from db import db
 import models
 from resources.item import blp as ItemBlueprint
@@ -24,12 +25,14 @@ def create_app(db_url=None):
         "DATABASE_URL", "sqlite:///data.db"
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = "259909252070246346255773166438973990601"
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
 
     api = Api(app)
+    jwt = JWTManager(app)
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
